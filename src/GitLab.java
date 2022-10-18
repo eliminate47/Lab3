d27af15e4a75ad1fe68ec607331f205654776333
Vladislav Karpov <eliminate14152@gmail.com>
2022-10-18 16:55:25 +0300

    Чтение файла и проверка данных на корректность и запись результатов проверки в новый, отдельный файл

--- src/GitLab.java
+++ src/GitLab.java
@@ -1,16 +1,34 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.*;
 
 public class GitLab {
+
     public static void main(String[] args){
-        System.out.println("Results of checking:");
-        System.out.println(checkWithRegExp("_@BEST"));
-        System.out.println(checkWithRegExp("vovan"));
-        System.out.println(checkWithRegExp("vo"));
-        System.out.println(checkWithRegExp("Z@OZA"));
+        BufferedReader br = null;
+        try{
+            File file = new File("lab3Output.txt");
+
+            if(!file.exists())
+                file.createNewFile();
+
+            PrintWriter pw = new PrintWriter(file);
+            pw.println("Результат проверки:");
+
+            br = new BufferedReader(new FileReader("lab3.txt"));
+            String line;
+            while ((line = br.readLine()) != null){
+                pw.println(checkWithRegExp(line));
+            }
+            System.out.println("Результаты проверки записаны в файле lab3Output.txt");
+            pw.close();
+        } catch(IOException e) {
+            System.out.print("Error!");
+        }
+
     }
     public static boolean checkWithRegExp(String userNameString){
-        Pattern p = Pattern.compile("^[a-z0-9_-]{3,15}$");
+        Pattern p = Pattern.compile("^(([А-Я]{1}[а-я]{3,8})[ ]([А-Я]{1}[а-я]{5,8}))[ ]*[|]([1-9][0-9])[ ]*[|](\\+\\d{1,3}[ ]?)?(\\((\\d{3})\\)[ ]?)?(\\d{3}[-]?)?(\\d{2}[-]?)?(\\d{2})?[ ]*[|][\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$");
         Matcher m = p.matcher(userNameString);
         return m.matches();
     }
